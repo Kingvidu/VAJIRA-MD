ad698e13fab4d24c6b814da93c9c486f7654db06
Kingvidu <Viduramadushan987@gmail.com>
2024-07-30 22:10:10 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
 global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=jU0kFCZJ#Q5TJ4u7PY0xPAHMOxPTf6UKIEkorGm24r80J6E1hQ-U' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=aUd3CDDD#ys8PzzZJmjGlpaCqcmXFwBF0ICowhBO_SldHrTUzmK8' ,
 travaSend: process.env.TRAVA_SEND || '25' ,   
 MODERATORS : process.env.MODERATORS === undefined ? "94772801923,94787820101" : process.env.MODERATORS ,    
 MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
